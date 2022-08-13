3a8996d6e5dbd9be8973cee590dacd38015c4ac9
Marcell Mihály <mihalymarcell86@gmail.com>
2022-08-13 23:51:30 +0100

    mobile success screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Card from "./components/Card";
 import CardDetailForm from "./components/CardDetailForm";
+// import Success from "./components/Success";
 
 function App() {
   return (
     <main>
       <Card />
       <CardDetailForm />
+      {/* <Success /> */}
     </main>
   );
 }
--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,15 @@
+import iconComplete from "../images/icon-complete.svg";
+import scss from "../styles/Success.module.scss";
+
 function Success() {
-  return <>Thank you! We've added your card details Continue</>;
+  return (
+    <>
+      <img src={iconComplete} alt="" className={scss.icon} />
+      <h1 className={scss.thank_you}>Thank you!</h1>
+      <p className={scss.paragraph}>We've added your card details</p>
+      <button className={scss.button}>Continue</button>
+    </>
+  );
 }
 
 export default Success;
