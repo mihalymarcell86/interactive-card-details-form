ec39d17c27907db6b49a098fa79d8ad2cba0e7a5
Marcell Mihály <mihalymarcell86@gmail.com>
2022-08-22 19:55:31 +0100

    desktop layout

--- src/components/Success.js
+++ src/components/Success.js
@@ -3,12 +3,12 @@ import scss from "../styles/Success.module.scss";
 
 function Success() {
   return (
-    <>
+    <div className={scss.wrapper}>
       <img src={iconComplete} alt="" className={scss.icon} />
       <h1 className={scss.thank_you}>Thank you!</h1>
       <p className={scss.paragraph}>We've added your card details</p>
       <button className={scss.button}>Continue</button>
-    </>
+    </div>
   );
 }
 
