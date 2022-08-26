7cb805eb952e3415357c58c0d373edc470a8a798
Marcell Mihály <mihalymarcell86@gmail.com>
2022-08-26 22:06:04 +0100

    responsive / fluid layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Card from "./components/Card";
-// import CardDetailForm from "./components/CardDetailForm";
-import Success from "./components/Success";
+import CardDetailForm from "./components/CardDetailForm";
+// import Success from "./components/Success";
 
 import scss from "./styles/App.module.scss";
-
-//TODO: limit separation of card illustrations
 
 function App() {
   return (
     <main className={scss.main}>
       <Card />
-      {/* <CardDetailForm /> */}
-      <Success />
+      <CardDetailForm />
+      {/* <Success /> */}
     </main>
   );
 }
