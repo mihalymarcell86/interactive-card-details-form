62a14c713e79e12523f692257b0e473457c1f3d9
Marcell Mihály <mihalymarcell86@gmail.com>
2022-08-26 22:28:54 +0100

    input field focus states

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -6,15 +6,19 @@ function CardDetailForm() {
       <div className={scss.inputs}>
         <div className={scss.name}>
           <label htmlFor="name">Cardholder Name</label>
-          <input type="text" placeholder="e.g. Jane Appleseed" id="name" />
+          <div className={scss.input_wrapper}>
+            <input type="text" placeholder="e.g. Jane Appleseed" id="name" />
+          </div>
         </div>
         <div className={scss.number}>
           <label htmlFor="number">Card Number</label>
-          <input
-            type="number"
-            placeholder="e.g. 1234 5678 9123 0000"
-            id="number"
-          />
+          <div className={scss.input_wrapper}>
+            <input
+              type="number"
+              placeholder="e.g. 1234 5678 9123 0000"
+              id="number"
+            />
+          </div>
         </div>
         <div>
           <fieldset className={scss.exp_date}>
@@ -22,35 +26,41 @@ function CardDetailForm() {
               <abbr title="Expiry">Exp.</abbr> Date{" "}
               <span aria-hidden="true">(MM/YY)</span>
             </legend>
-            <input
-              type="number"
-              placeholder="MM"
-              min="1"
-              max="12"
-              aria-label="month of expiry"
-              id="exp_month"
-            />
-            <input
-              type="number"
-              placeholder="YY"
-              aria-label="year of expiry"
-              id="exp_year"
-              max="99"
-              min="00"
-            />
+            <div className={scss.input_wrapper}>
+              <input
+                type="number"
+                placeholder="MM"
+                min="1"
+                max="12"
+                aria-label="month of expiry"
+                id="exp_month"
+              />
+            </div>
+            <div className={scss.input_wrapper}>
+              <input
+                type="number"
+                placeholder="YY"
+                aria-label="year of expiry"
+                id="exp_year"
+                max="99"
+                min="00"
+              />
+            </div>
           </fieldset>
         </div>
         <div className={scss.cvc}>
           <label htmlFor="cvc">
             <abbr title="Card Verification Code">CVC</abbr>
           </label>
-          <input
-            type="number"
-            placeholder="e.g. 123"
-            id="cvc"
-            max="999"
-            min="0"
-          />
+          <div className={scss.input_wrapper}>
+            <input
+              type="number"
+              placeholder="e.g. 123"
+              id="cvc"
+              max="999"
+              min="0"
+            />
+          </div>
         </div>
       </div>
       <button className={scss.button}>Confirm</button>
